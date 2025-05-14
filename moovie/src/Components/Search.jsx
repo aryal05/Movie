55bd2522448b1b23696ffb68538aa70ef8ef2a90
aryal05 <aryal.rajat05@gmail.com>
2025-05-14 20:58:21 +0545

    api key stored

--- moovie/src/Components/Search.jsx
+++ moovie/src/Components/Search.jsx
@@ -4,11 +4,13 @@ const Search = ({searchTerm, setSearchTerm}) => {
   return (
     <div className='w-[655px] h-12 bg-blue-950'>
       <div className='flex items-center w-full h-full ml-3 '>
-        <img src="search.svg" className='text-blue-800' alt="" />
+        <img src="search.svg" className='text-blue-800 cursor-pointer' alt="" />
         <input type="text"
-        className='border-none outline-0 text-white ml-5
+        className='border-none w-[500px] outline-0 text-white ml-5
         '
-        placeholder='Search Here..' />
+        placeholder='Search Through Thousand of Movies'
+        value={searchTerm}
+        onChange={(e)=> setSearchTerm(e.target.value)} />
       </div>
     </div>
   )
