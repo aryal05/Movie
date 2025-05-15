c0be536d5864b6e13ddac4273ad89225cbf6829f
aryal05 <aryal.rajat05@gmail.com>
2025-05-15 20:21:19 +0545

    movies tittle fettching

--- moovie/src/App.jsx
+++ moovie/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, use } from 'react';
 import Search from './Components/Search'
 
 const API_BASE_URL = 'https://api.themoviedb.org/3'
@@ -17,12 +17,15 @@ const API_OPTIONS = {
 const App = () => {
   const [searchTerm, setSearchTerm] = useState('')
   const [error, setError] = useState('')
+  const [movieList, setMovieList] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
  const fetchMovies = async () => {
+  setIsLoading(true)
   try {
     const endPoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
     const response = await fetch(endPoint, API_OPTIONS);
     
-    // console.log("HTTP Status:", response.status); // log status code
+    // console.log("HTTP Status:", response.status); // log status code 
     // console.log("Response:", response); // log response object
 
     if (!response.ok) {
@@ -31,9 +34,16 @@ const App = () => {
 
     const data = await response.json();
     console.log(data);
+    // if(data.Response == false){
+    //   throw new Error("Failed to Shoe Movies Data.")
+
+    // }
+    setMovieList(data.results || [])
   } catch (error) {
     console.error("Error fetching movies:", error.message, error);
     setError("Error Fetching Movies. Please Try Again.");
+  } finally{
+    setIsLoading(false)
   }
 };
 
@@ -51,6 +61,14 @@ const App = () => {
         <Search searchTerm = {searchTerm} setSearchTerm = {setSearchTerm}/>
         <section>
         <h2 className='text-white'>All Movies</h2>
+        {isLoading?(<p className='text-fuchsia-600 text-2xl' >Loading Please Wait..</p>): error? (<p className='text-red-600 text-2xl'>Error Showing Movies.</p>):
+        (<ul>
+          {movieList.map((movie)=>{
+            return(
+              <p className='text-white'>{movie.title}</p>
+            )
+          })}
+        </ul>)}
       </section>
       </div>
       
